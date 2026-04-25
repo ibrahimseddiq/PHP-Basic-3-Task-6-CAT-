af7bbaf222c36c7019e47e2c5c4d3d3a1c3e1e67
ibrahimseddiq <ibrahimgheit84@gmail.com>
2026-04-25 06:50:40 +0300

    fix logic for consecutive check

--- ConsecutiveCombo.php
+++ ConsecutiveCombo.php
@@ -1,10 +1,12 @@
 <?php
 function consecutive_combo($arr1, $arr2) {
-    $merged_arrays = array_merge($arr1, $arr2); // Supposed that ther is no Duplicated Numbers..
+    $merged_arrays = array_merge($arr1, $arr2); 
     sort($merged_arrays);
     for($i = 0; $i < count($merged_arrays) - 1; $i++) {
-        if($merged_arrays[$i] + 1 != $merged_arrays[$i + 1])
+        if($merged_arrays[$i] != $merged_arrays[$i + 1] && $merged_arrays[$i] + 1 != $merged_arrays[$i + 1]) // In Case of there is a Duplicated Numbers.
             return false;
+        // if($merged_arrays[$i] + 1 != $merged_arrays[$i + 1]) // Supposed that ther is no Duplicated Numbers..
+        //     return false;
     }
     return true;
 }
@@ -12,3 +14,4 @@ var_dump(consecutive_combo([7, 4, 5, 1], [2, 3, 6]));
 var_dump(consecutive_combo([1, 4, 6, 5], [2, 7, 8, 9]));
 var_dump(consecutive_combo([1, 4, 5, 6], [2, 3, 7, 8, 10]));
 var_dump(consecutive_combo([44, 46], [45]));
+var_dump(consecutive_combo([1,2,3],[3,4])); // Duplicated Numbers.
